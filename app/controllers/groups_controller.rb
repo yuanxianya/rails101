ab53c44e29d373492ff4ebf5749b44676cfb4d99
小兽小兽 <yuanxianya@gmail.com>
2016-12-21 16:15:29 +0800

    show group's creator info

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create]
   def index
     @groups = Group.all
   end
@@ -6,7 +7,16 @@ class GroupsController < ApplicationController
   def new
  @group = Group.new
 end
+def create
+    @group = Group.new(group_params)
+    @group.user = current_user
 
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
 def show
   @group = Group.find(params[:id])
 end
