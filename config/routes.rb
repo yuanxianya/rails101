abe1972daeb47fcdf0832571654643e1c99d3369
小兽小兽 <yuanxianya@gmail.com>
2016-12-21 13:54:36 +0800

    add posts nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-resources :groups
-   root 'groups#index'
-end
+  resources :groups do
+      resources :posts
+    end
+    root 'groups#index'
+  end
