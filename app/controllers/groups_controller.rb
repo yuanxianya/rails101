495157afdba2a364131b323ddd7d0939f58953da
小兽小兽 <yuanxianya@gmail.com>
2016-12-21 15:29:10 +0800

    move form to partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,11 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
+
   def new
  @group = Group.new
+end
 end
 def show
   @group = Group.find(params[:id])
