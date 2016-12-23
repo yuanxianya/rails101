9261cd7a55673b8c8544f9c8a17a9f03635ef8e2
小兽小兽 <yuanxianya@gmail.com>
2016-12-23 16:23:39 +0800

    use simple_format helper to decorate

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 		resources :groups
 		resources :posts
 	end
+
 	root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
